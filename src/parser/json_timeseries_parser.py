be864342de1282c1eb96e6f9ac536f2bd403e702
LeeJeongSeok <ljs0429777@gmail.com>
2025-04-23 14:31:37 +0900

    refactor: 분리했던 db 커넥션 코드 원복

--- src/parser/json_timeseries_parser.py
+++ src/parser/json_timeseries_parser.py
@@ -4,7 +4,8 @@ import pytz
 import requests
 
 from datetime import datetime
-from src.utils.insert_db import insert_to_db
+from sqlalchemy import create_engine
+from sshtunnel import SSHTunnelForwarder
 
 # -----------------------------
 # 기능 함수
@@ -37,6 +38,33 @@ def parse_api_response(response):
         data.append(row)
 
     return pd.DataFrame(data)
+
+# ssh 연결
+def connect_ssh_tunnel(ssh_config):
+    tunnel = SSHTunnelForwarder(
+        (ssh_config['ssh_host'], ssh_config['ssh_port']),
+        ssh_username=ssh_config['ssh_username'],
+        ssh_private_key=ssh_config['ssh_private_key'],
+        remote_bind_address=(
+            ssh_config['remote_bind_address']['host'],
+            ssh_config['remote_bind_address']['port']
+        ),
+        local_bind_address=(
+            ssh_config['local_bind_address']['host'],
+            ssh_config['local_bind_address']['port']
+        )
+    )
+    tunnel.start()
+    return tunnel
+
+
+def insert_to_db(df, db_config, ssh_config):
+    tunnel = connect_ssh_tunnel(ssh_config)
+    engine = create_engine(
+        f"mysql+pymysql://{db_config['user']}:{db_config['password']}@127.0.0.1:{ssh_config['local_bind_address']['port']}/{db_config['name']}"
+    )
+    df.to_sql(name="KETI_BASE_TB", con=engine, if_exists="append", index=False)
+    tunnel.close()
 
 
 def call_keti_aws_api(config: dict):
